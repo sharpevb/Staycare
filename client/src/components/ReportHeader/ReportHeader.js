c84815e0a2e9dd640c8006c04528238a82289880
sharpevb <victoriabsharpe@gmail.com>
2019-10-17 00:54:19 -0400

    report header displaying data with less code

--- client/src/components/ReportHeader/ReportHeader.js
+++ client/src/components/ReportHeader/ReportHeader.js
@@ -6,27 +6,26 @@ import "./style.css";
 class ReportHeader extends Component {
 
     state = {
-        reportheader: {
+        kid: {
             id: "",
             name: "",
             dob: "",
             allergies: "",
             medication: "",
-            primaryphone: "",
             image: "",
-            familyname: "",
         },
         familyProfile: {
             id: "",
-            familyname: "",
+            surname: "",
             primaryphone: "",
         }
     }
 
     componentDidMount() {
+        //Eddie Info
         API.findMemberById(3)
             .then(res => {
-                let reportheader = {
+                let kid = {
                     id: res.data.id,
                     name: res.data.name,
                     dob: res.data.dob,
@@ -35,22 +34,25 @@ class ReportHeader extends Component {
                     image: res.data.image,
 
                 };
-                this.setState({ reportheader: reportheader })
+                this.setState({ kid: kid })
             })
             .catch(err => console.log(err));
 
-        API.findFamilyById(1)
+            // Family members and info
+            API.findMembersByFamily(1)
             .then(res => {
-                console.log(res);
+                console.log(res)
                 let familyProfile = {
-                    id: res.data.id,
-                    familyname: res.data.familyname,
-                    primaryphone: res.data.primaryphone,
+                    child1: res.data[0].name,
+                    parent1: res.data[1].name,
+                    parent2: res.data[2].name,
+                    surname: res.data[0].Family.familyname,
+                    primaryphone: res.data[1].phone
                 };
-                this.setState({ familyProfile: familyProfile });
+                this.setState({familyProfile: familyProfile});
                 console.log(familyProfile);
             })
-            .catch(err => console.log(err));
+    
 
     }
 
@@ -63,24 +65,24 @@ class ReportHeader extends Component {
                         <div className="row" id="banner-row">
 
                             <div className="col-2">
-                                <img className="image" alt={this.state.reportheader.image} src={this.state.reportheader.image}></img>
+                                <img className="image" alt={this.state.kid.image} src={this.state.kid.image}></img>
                             </div>
                             <div className="col">
-                                <h1>{this.state.reportheader.name} {this.state.familyProfile.familyname}</h1>
+                                <h1>{this.state.kid.name} {this.state.familyProfile.surname}</h1>
                                 <div className="row">
                                     <div className="col-4">
-                                        <p><strong>Date of Birth:</strong> {this.state.reportheader.dob}</p>
+                                        <p><strong>Date of Birth:</strong> {this.state.kid.dob}</p>
                                     </div>
                                     <div className="col-6">
-                                        <p><strong>Parent/Guardian(s):</strong> </p>
+                                        <p><strong>Parent/Guardian(s): </strong>{this.state.familyProfile.parent1} {this.state.familyProfile.surname}, {this.state.familyProfile.parent2} {this.state.familyProfile.surname}</p>
                                     </div>
                                 </div>
                                 <div className="row">
                                     <div className="col-4">
-                                        <p><strong>Allergies:</strong> {this.state.reportheader.allergies}</p>
+                                        <p><strong>Allergies:</strong> {this.state.kid.allergies}</p>
                                     </div>
                                     <div className="col-3">
-                                        <p><strong>Phone:</strong> </p>
+                                        <p><strong>Phone: </strong>{this.state.familyProfile.primaryphone}</p>
                                     </div>
                                 </div>
                             </div>
