43f5d622983703eb83a42b3207433930ce05bfd6
dhall <darrenjhall4@gmail.com>
2019-10-22 16:48:34 -0400

    styling changes to ProfileCardEdit, ProfileCardReadOnly, FamilyProfileEdit, FamilyProfileReadOnly

--- client/src/components/FamilyProfile/FamilyProfileEdit.js
+++ client/src/components/FamilyProfile/FamilyProfileEdit.js
@@ -1,33 +1,62 @@
 import React from "react";
+import "./style.css";
 
 function FamilyProfileEdit(props) {
   return (
-    <div className="card">
-      <div className="img-container">
-        <img alt={props.image} src={props.image}/>
-      </div>
-      <div className="content">
-        <ul>
-          <li>
-            <strong>Edit:</strong> <input value={props.familyname}  name="name" onChange={props.handleInputChangeFamily}/>
-          </li>
-          <li>
-            <strong>Family ID:</strong> <input value={props.id} name="id" onChange={props.handleInputChangeFamily}/>
-          </li>
+    <div>
+      <div className="card-body" id="familyProfileEdit">
+        <div className="card-body">
+          <p>
+            <strong>Edit:</strong>{" "}
+            <input
+              value={props.familyname}
+              name="name"
+              onChange={props.handleInputChangeFamily}
+            />
+          </p>
 
-          <li>
-            <strong>Primary Phone:</strong> <input value={props.primaryphone} name="primaryphone" onChange={props.handleInputChangeFamily}/>
-          </li>
-          <li>
-            <strong>Primary Address:</strong> <input value={props.address1} name="address1" onChange={props.handleInputChangeFamily}/>
-          </li>
-          <li>
-            <strong>Secondary Address:</strong> <input value={props.address2} name="address2" onChange={props.handleInputChangeFamily}/>
-          </li>
-        </ul>
-        <button onClick={() => props.deleteFamily(props.id)} className="remove">
-          Delete Family
-        </button>
+          <p>
+            <strong>Family Name:</strong>{" "}
+            <input
+              value={props.familyname}
+              name="name"
+              onChange={props.handleInputChangeFamily}
+            />
+          </p>
+          <p>
+            <strong>Family ID:</strong>{" "}
+            <input
+              value={props.id}
+              name="id"
+              onChange={props.handleInputChangeFamily}
+            />
+          </p>
+
+          <p>
+            <strong>Primary Phone:</strong>{" "}
+            <input
+              value={props.primaryphone}
+              name="primaryphone"
+              onChange={props.handleInputChangeFamily}
+            />
+          </p>
+          <p>
+            <strong>Primary Address:</strong>{" "}
+            <input
+              value={props.address1}
+              name="address1"
+              onChange={props.handleInputChangeFamily}
+            />
+          </p>
+          <p>
+            <strong>Secondary Address:</strong>{" "}
+            <input
+              value={props.address2}
+              name="address2"
+              onChange={props.handleInputChangeFamily}
+            />
+          </p>
+        </div>
       </div>
     </div>
   );
