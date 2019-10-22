23e636fa7e250c191c30d2fb0ab47970ca633349
sharpevb <victoriabsharpe@gmail.com>
2019-10-22 16:48:15 -0400

    rest of RO elements

---
+++ client/src/components/DailyReport/DRcomps/DR_meds/DRmedsRO.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./style.css";
+
+
+function DRmedsRO() {
+
+    return (
+
+        <div className="row" id="container">
+            <div className="col-3" id="med-section">
+                <label>Medications administered: </label>
+            </div>
+
+            <div className="col-5">
+                <p>N/A</p>
+            </div>
+
+        </div>
+    )
+
+
+}
+
+export default DRmedsRO;
---
+++ client/src/components/DailyReport/DRcomps/DR_sleep/DRsleepRO.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import API from "../../../../utils/API"
+import "./style.css";
+
+
+
+class DRsleep extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            activity6: {
+                memberId: "",
+                activitydate: "",
+                activitycode: "",
+                time: "",
+                result1: "",
+                result2: ""
+            },
+
+        }
+
+    }
+
+    componentDidMount() {
+        API.findActivityById(6)
+        .then(res => {
+            let activity6 = {
+                memberId: 3,
+                activitycode: res.data.activitycode,
+                activitydate: res.data.activitydate,
+                time: res.data.time,
+                result1: res.data.result1,
+                result2: res.data.result2,
+            };
+            this.setState({ activity6: activity6 });
+            console.log(activity6);
+
+
+        })
+        .catch(err => console.log(err));
+    }
+
+render() {
+    return (
+        <div className="container-fluid">
+        <div className="row" id="container">
+            <div className="col-3">
+                <label>Sleep: </label>
+            </div>
+            <div className="col-7">
+                <p>Time: {this.state.activity6.time} to {this.state.activity6.result1}</p>
+            </div>
+        </div>
+        </div>
+    )
+}
+    
+
+}
+
+
+export default DRsleep;
